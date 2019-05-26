c9ebe08f315f9f6745ea29bebd1f7b9579b10d5e
santiagocaballier <caballiersantiago@hotmail.com>
2019-05-26 00:31:08 -0300

    ubicaciones

--- gentelella/sensor/urls.py
+++ gentelella/sensor/urls.py
@@ -3,7 +3,8 @@ from sensor import views
 
 urlpatterns = [
     path('setdata/<str:_mac_address>/<int:_dataint>.<int:_datadec>',views.set_data,name='sensor_setdata'),
-    path('refresh_graph/<str:_sensor_name>/', views.refresh_graph, name = 'refresh_graph')
+    path('refresh_graph/<str:_sensor_name>/', views.refresh_graph, name = 'refresh_graph'),
+    path('refresh_ubicacion/<str:_ubicacion_name>/', views.refresh_ubicacion, name = 'refresh_ubicacion')
     #path('test/', views.test, name='test')
     
 ]
--- gentelella/sensor/views.py
+++ gentelella/sensor/views.py
@@ -77,3 +77,58 @@ def refresh_graph(request,_sensor_name):
 
     
     return JsonResponse(graph_data,safe=False)
+
+def refresh_ubicacion(request,_ubicacion_name):
+    if request.GET['start_date']:
+        start_date = request.GET['start_date']
+    else:
+        start_date = datetime.now().strftime("%Y-%m-%d 00:00")
+    if request.GET['end_date']: 
+        end_date = request.GET['end_date']
+    else:
+        end_date = datetime.now().strftime("%Y-%m-%d 23:59")
+    
+    
+    #Devolver una  tabla en Json con cada sensor y validacion de alarma.
+    '''
+    #    Calidad de aire    Temperatura    Luminosidad    Intensidad sonora    Factor de cumplimiento
+    1    Dato 1    Dato 2    Dato 3    Dato 4    0%
+    2    Dato 1    Dato 2    Dato 3    Dato 4    0%
+    3    Dato 1    Dato 2    Dato 3    Dato 4    0%
+    4    Dato 1    Dato 2    Dato 3    Dato 4    0%
+    
+    '''
+    
+
+    jssensores = {}
+    #Obtengo los sensores
+    dqsensor = Sensor.objects.filter(ubicacion__nombre = _ubicacion_name)
+    obj_sensores = []
+    if dqsensor:
+        for s in dqsensor:
+            datas = {"sensor" : s.nombre, "tipo" : s.tipo.tipo}
+            
+            #datad = {"datetime" : "01/01/1900","value" : "12"}
+            dq = DataSensor.objects.filter(sensor = s, datetime__range=(start_date, end_date))
+            obj_data = []
+            if dq:
+                for d in dq:
+                    datad = {"datetime" : str(d.datetime.date()) + ' ' + str(d.datetime.time()) , "value" : str(d.data) , "alarma": str(d.alarma_value)}
+                    obj_data.append(datad)
+            else:
+                datad = {}
+   
+            
+            datas = {"sensor" : s.nombre, "tipo" : s.tipo.tipo ,"data":obj_data}
+            obj_sensores.append(datas)
+    else:
+        datas = {}
+    
+    jssensores.update({"sensores" : obj_sensores})
+    
+    #Json    
+    table_data = json.dumps(jssensores)
+    
+    
+    return JsonResponse(table_data,safe=False)
+
