e8cf57ea2f0bdbeeec0cb5121b434620819cba59
santiagocaballier <caballiersantiago@hotmail.com>
2019-06-22 19:44:14 -0300

    Revision final

--- gentelella/sensor/admin.py
+++ gentelella/sensor/admin.py
@@ -3,8 +3,8 @@ from .models import Sensor,DataSensor,Empresa,Alarma,Ubicacion,Tipo
 
 # Register your models here.
 admin.site.register(Sensor)
-admin.site.register(DataSensor)
+#admin.site.register(DataSensor)
 admin.site.register(Empresa)
 admin.site.register(Alarma)
 admin.site.register(Ubicacion)
-admin.site.register(Tipo)
+
